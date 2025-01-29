c3bfd79773a42cc60dbdb88c82416c9976b95489
Tom Sabu <tomsabu0000@gmail.com>
2025-01-29 19:24:31 +0530

    add Tailwind CSS setup with PostCSS configuration and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
