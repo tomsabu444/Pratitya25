17689783e8383cc61e8ab52ab86bdeac417f4298
TOM SABU <tomsabu0000@gmail.com>
2025-01-30 10:57:06 +0530

    add TeamPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventList from "./pages/EventList";
 import EventDetails from "./pages/EventDetails";
 import HomePage from "./pages/HomePage";
+import TeamPage from "./pages/TeamPage";
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Route path="/events" element={<EventList />} />
         {/* Event Details Page */}
         <Route path="/event/:id" element={<EventDetails />} />
+        {/* Team Page */}
+        <Route path="/teams" element={<TeamPage/>} />
       </Routes>
     </Router>
   );
