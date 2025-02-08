c42dbeb6b675a05d57c7aa4ce2a77eae4464425d
Aleena P Mathews <aleenapmathews2004@gmail.com>
2025-02-08 23:06:23 +0530

    made changes in size of image

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -11,31 +11,29 @@ const EventList = () => {
   };
 
   return (
-    <div className=" relative p-6 flex flex-col items-center  w-full min-h-screen overflow-hidden mesh-gradient  ">
-<Animation/>
-      <h1 className="text-white text-center text-5xl md:text-9xl font-camodirt  mb-10">
+    <div className="relative p-6 flex flex-col items-center justify-start w-full min-h-screen overflow-hidden mesh-gradient">
+      <Animation />
+      <h1 className="text-white text-center text-5xl md:text-9xl font-camodirt mb-10 relative z-10">
         EVENTS
       </h1>
-      <div className=" grid grid-cols-1 gap-y-7 md:gap-x-56 md:gap-y-20 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-y-7 md:gap-x-24 md:gap-y-12 lg:gap-x-56 lg:gap-y-20 lg:grid-cols-3 md:grid-cols-2">
         {events.map((event) => (
           <div
             key={event.id}
             onClick={() => handleEventClick(event.id)}
-            className="bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition-transform w-80"
+            className="bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition-transform w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
           >
-          
             <img
               src={event.poster_url}
               alt={`${event.name} Poster`}
-              className="w-full h-auto object-cover"
+              className="w-full h-auto max-h-[400px] object-cover"
             />
           </div>
         ))}
       </div>
-      </div>
-    
-    
+    </div>
   );
 };
 
 export default EventList;
+
