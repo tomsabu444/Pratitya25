b12a128fa423feb1e108f8d9f9c59888d9ff2b9e
Jishnu <jishnumohan292@gmail.com>
2025-02-11 11:03:57 +0530

    cc c#

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend:{
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        camodirt: ["CAMODIRT-Regular", "sans-serif"],
-        rancho: ["Rancho", "cursive"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
