877ad69bb3018ddd07bcd4f4bf2eac646512442f
Jishnu <jishnumohan292@gmail.com>
2025-02-15 21:37:57 +0530

    paralax for lanten added

--- src/components/Homeone.jsx
+++ src/components/Homeone.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import background from "../assets/home-section-one/home-bg.png";
 import lanten from "../assets/home-section-one/lantengroup.png";
 
-export const HomeOne = () => {
+const HomeOne = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="relative">
       {/* Background image - 2 viewport heights */}
@@ -15,12 +29,15 @@ export const HomeOne = () => {
       />
       
       {/* Overlay image - 1 viewport height */}
-      <div 
+      <div
         className="absolute top-0 left-0 h-screen w-full flex items-center justify-center"
+        style={{
+          transform: `translateY(${scrollPosition * -0.75}px)` // Changed to negative value for upward movement
+        }}
       >
-        <img 
-          src={lanten} 
-          alt="Lanten Group" 
+        <img
+          src={lanten}
+          alt="Lanten Group"
           className="w-screen h-auto object-contain"
         />
       </div>
